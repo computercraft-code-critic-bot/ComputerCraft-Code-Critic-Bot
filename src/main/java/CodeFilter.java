92d9091ec73989f52a0d0fa1a9bc24ed5f08d373
Lupus590 <lupussolitarius590@gmail.com>
2019-03-27 22:46:26 +0000

    ideas

---
+++ src/main/java/CodeFilter.java
@@ -0,0 +1,13 @@
+//takes discord message as input and strips out everything that is not in code tags
+
+public class CodeFilter {
+
+    public static string filter(string rawMessage)
+    {
+        //todo
+        string code = rawMessage;
+
+        return code;
+    }
+
+}
--- src/main/java/MessageListener.java
+++ src/main/java/MessageListener.java
@@ -19,10 +19,9 @@ public class MessageListener extends ListenerAdapter
         String content = message.getContentRaw();
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-        if (content.equals("!ping"))
-        {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-        }
+
+        System.out.println(message.getContentRaw());
+        System.out.println(message.getContentDisplay());
+
     }
 }
